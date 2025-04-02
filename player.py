ff82cfcd256b5923ff356db6cc939581e540d04f
thalestrcs <thalesr187@gmail.com>
2025-04-01 22:34:50 -0300

    Melhorei a movimentação, mas fiz as alterações apenas no arquivo  player.py, posso passar para o arquivo player que está sendo usado no jogo depois

--- player.py
+++ player.py
@@ -45,38 +45,64 @@ class Player(pygame.sprite.Sprite):
                 self.vel_y = 0
                 self.on_ground = True
 =======
-import pygame 
+import pygame
+from pygame.locals import *
+from settings import WHITE, ALTURA, LARGURA
 
-class Player:
-    def __init__(self, x_player, y_player):
-        self.hp = 100 
-        self.velocidade = 10
-        self.x = x_player
-        self.y = y_player
-        self.quadrado = pygame.Rect(self.x, self.y, 20, 20) 
+# Classe do jogador
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((40, 50))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.vel_y = 0
+        self.on_ground = False
+    
+    def update(self, platforms):
+        keys = pygame.key.get_pressed()
+        if keys[K_LEFT] or keys[K_a]:
+            self.rect.x -= 5
+            for plat in platforms:
+                if self.rect.colliderect(plat):  
+                    self.rect.x += 5  # Reverte o movimento
+        if keys[K_RIGHT] or keys[K_d]:
+            self.rect.x += 5
+            for plat in platforms:
+                if self.rect.colliderect(plat):  
+                    self.rect.x -= 5  # Reverte o movimento
+            
+        self.vel_y += 1
+        self.rect.y += self.vel_y  
+        self.on_ground = False  
 
-    def mover(self, largura_mapa):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_a] or key[pygame.K_LEFT]:
-            self.x -= self.velocidade 
-        if key[pygame.K_d] or key[pygame.K_RIGHT]:
-            self.x += self.velocidade 
-
-        self.quadrado.x = self.x
-        self.quadrado.y = self.y
+        # Colisão com as plataformas
+        for plat in platforms:
+            if self.rect.colliderect(plat):
+                if self.vel_y > 0:  # Se estiver caindo
+                    self.rect.bottom = plat.rect.top  
+                    self.vel_y = 0  
+                    self.on_ground = True  # Confirma que está no chão
+                elif self.vel_y < 0:  # Se bater a cabeça
+                    self.rect.top = plat.rect.bottom
+                    self.vel_y = 0
+        
+        # Colisão com o chão
+        if self.rect.bottom > ALTURA:
+            self.rect.bottom = ALTURA
+            self.vel_y = 0
+            self.on_ground = True
+        # Fazendo com que o jogador não saia da tela
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > LARGURA:
+            self.rect.right = LARGURA 
 
     def pular(self):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_w] or key[pygame.K_SPACE]:
-            self.y -= 40 
-        self.quadrado.y = self.y
-
-    def gravidade(self):
-        self.y += 10
-        self.quadrado.y = self.y
-    
-    def desenhar(self, tela):
-        pygame.draw.rect(tela, (0, 255, 0), self.quadrado)
+        keys = pygame.key.get_pressed()
+        if (keys[K_SPACE] or keys[K_w] or keys[K_UP]) and self.on_ground:
+            self.vel_y = -15
 
 
 
