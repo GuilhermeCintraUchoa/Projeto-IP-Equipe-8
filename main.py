190ad04455f4a888ea5ecff025c079cd4ae9badf
josebrunos <jbnos4@gmail.com>
2025-04-07 13:54:48 -0300

    Update main.py
    Unificando os arquivos

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from settings import LARGURA, ALTURA, FPS, GREEN, BLACK
 from player_andrews import Player
 from platform_andrews import Platform 
 from enemy import Enemy
+from sword_hitbox import SwordHitbox
 
 # Inicializa o Pygame
 pygame.init()
@@ -22,11 +23,15 @@ platforms = pygame.sprite.Group()
 platforms.add(Platform(200, 350, 200, 20))
 platforms.add(Platform(450, 250, 200, 20))
 platforms.add(Platform(130, 150, 200, 20))
-enemy = Enemy(130, 100, 130, 370)
+enemy_1 = Enemy(130, 100, 130, 370, vida=27)
+enemy_2 = Enemy(450, 200, 450, 700, vida=100)
+enemies = pygame.sprite.Group()
+enemies.add(enemy_1)
+enemies.add(enemy_2)
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
-all_sprites.add(enemy)
+all_sprites.add(enemies)
 all_sprites.add(*platforms)
 
 # Loop principal
@@ -36,18 +41,32 @@ going = True
 while going:
     clock.tick(FPS)
     
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             going = False
-    
+         
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_n:
+                player.attack(keys)
+
+    player.sword.update()
     player.update(platforms)
-    enemy.update()
+    enemies.update()
 
-    if player.colisao_inimigo(enemy) and enemy.vida > 0:
-        enemy.vida -= 1
-        enemy.die()
-        kills += 1
+    if player.sword.active:
+        hits = pygame.sprite.spritecollide(player.sword, enemies, dokill=False)
+        for enemy in hits:
+            enemy.levar_dano(player.sword.dano)  # Aplica dano ao inimigo
+            if enemy.vida <= 0:
+                enemy.kill()
+                kills += 1
 
+    for enemy in enemies:
+        if player.colisao_inimigo(enemy) and enemy.vida > 0:
+            enemy.die()  # Mata o inimigo instantaneamente
+            kills += 1
+            
     player.morte()
     player.sofreu_dano(all_sprites)
 
@@ -57,6 +76,9 @@ while going:
     mensagem = fonte.render(f"Monstros mortos: {kills}", True, BLACK)
     screen.blit(mensagem, (400, 10))
     all_sprites.draw(screen)
+
+    if player.sword.active:
+        screen.blit(player.sword.image, player.sword.rect.topleft)
 
     
     pygame.display.flip()
