5864e09ef29a4be4a29a2a8260929521df1ed04e
Guilherme Cintra <cintratop@gmail.com>
2025-04-07 09:51:41 -0300

    Implementacao dos menus

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from settings import LARGURA, ALTURA, FPS, GREEN, BLACK
 from player_andrews import Player
 from platform_andrews import Platform 
 from enemy import Enemy
+import sys
 
 # Inicializa o Pygame
 pygame.init()
@@ -31,36 +32,106 @@ all_sprites.add(player)
 all_sprites.add(enemies)
 all_sprites.add(*platforms)
 
+# Funcao draw_text
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+click = False
+
+def main_menu ():
+    while True:
+
+        screen.fill((0, 0, 0))
+        draw_text('main menu', fonte, (255, 255, 255), screen, 20, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+        
+        pygame.display.update()
+        clock.tick(FPS)
+
+
 # Loop principal
 
-kills = 0
-going = True
-while going:
-    clock.tick(FPS)
-    
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            going = False
-    
-    player.update(platforms)
-    enemy.update()
+def game():
+    kills = 0
+    going = True
+    while going:
+        screen.fill((0,0,0))
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                going = False
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    going = False
+                    
+        
+        player.update(platforms)
+        enemy.update()
 
-    if player.colisao_inimigo(enemies) and enemy.vida > 0:
-        enemy.vida -= 1
-        enemy.die()
-        kills += 1
+        if player.colisao_inimigo(enemies) and enemy.vida > 0:
+            enemy.vida -= 1
+            enemy.die()
+            kills += 1
 
-    player.morte()
-    player.sofreu_dano(all_sprites)
+        player.morte()
+        player.sofreu_dano(all_sprites)
 
-    # Desenhar na tela
-    screen.fill(GREEN)
-    screen.blit(fundo, (0,0))
-    mensagem = pygame.transform.scale_by((fonte.render(f"Monstros mortos: {kills}", True, BLACK)), 0.7)
-    screen.blit(mensagem, (500, 10))
-    all_sprites.draw(screen)
- 
-    pygame.display.flip()
-    
+        # Desenhar na tela
+        screen.fill(GREEN)
+        screen.blit(fundo, (0,0))
+        mensagem = pygame.transform.scale_by((fonte.render(f"Monstros mortos: {kills}", True, BLACK)), 0.7)
+        screen.blit(mensagem, (500, 10))
+        all_sprites.draw(screen)
+        pygame.display.update()
+
+def options():
+    going = True
+    while going:
+        screen.fill((0,0,0))
+        draw_text('options', fonte, (255, 255, 255), screen, 20, 20)
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                going = False
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    going = False
+        pygame.display.update()
+
+
+main_menu()
     
 pygame.quit()
