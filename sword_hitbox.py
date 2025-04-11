e99fdc205a6a01b96e44ff799d260e7dbe47223c
josebrunos <jbnos4@gmail.com>
2025-04-10 22:48:48 -0300

    Update sword_hitbox.py

--- sword_hitbox.py
+++ sword_hitbox.py
@@ -6,7 +6,7 @@ class SwordHitbox(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, dano=1):
         super().__init__()
         self.image = pygame.Surface((width, height), pygame.SRCALPHA)
-        self.image.fill((255, 0, 0, 100))
+        self.image.fill((0, 0, 0, 0))
         self.rect = self.image.get_rect(topleft=(x, y))
         self.active = False
         self.timer = 0
@@ -14,9 +14,9 @@ class SwordHitbox(pygame.sprite.Sprite):
 
     def activate(self, x, y, direction):
         if not direction:  # Para esquerda
-            self.rect.topleft = (x - 40, y - 10)
+            self.rect.topleft = (x - 20, y - 5)
         else:  # Para direita
-            self.rect.topleft = (x + 40, y - 10)
+            self.rect.topleft = (x + 30, y - 5)
         self.active = True
         self.timer = 10
 
